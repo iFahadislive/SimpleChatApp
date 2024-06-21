f927b81ee8b694d5ec566e37221d4ccfa065484d
Fahad Safdar <iFahadislive@gmail.com>
2024-06-21 18:30:49 +0500

    re-creating app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:chat_app/controllers/auth_gate.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/theme/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'core/navigation/navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,10 +19,6 @@ class ChatApp extends StatefulWidget {
 class _ChatAppState extends State<ChatApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: const AuthGate(),
-        theme: lightMode,
-        themeMode: ThemeMode.system,
-        routes: routes);
+    return MaterialApp();
   }
 }
