f90881255967dc4275b42b5fa34c6d051cbad6d2
Fahad Safdar <iFahadislive@gmail.com>
2024-06-21 18:31:38 +0500

    re-creating app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/firebase_options.dart';
-import 'package:chat_app/theme/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
